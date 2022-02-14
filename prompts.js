7549f78d4530b49e7ffb5f37beab17e2a2d69af7
xu_lw <xu_lw@murongtech.com>
2022-02-14 17:49:42 +0800

    delete choice

--- prompts.js
+++ prompts.js
@@ -1,36 +1,11 @@
 module.exports = [{
   type: 'list',
   name: 'template',
-  message: 'Please select a uni-app template',
+  message: 'Please select a miniapp template',
   choices: [{
     name: 'Default template',
     value: 'default'
   },
-  {
-    name: 'Default template(TypeScript)',
-    value: 'default-ts'
-  },
-  {
-    name: 'Hello uni-app',
-    value: 'dcloudio/hello-uniapp'
-  },
-  {
-    name: 'Front and back login template',
-    value: 'dcloudio/uni-template-login'
-  },
-  {
-    name: 'Look at the picture template',
-    value: 'dcloudio/uni-template-picture'
-  },
-  {
-    name: 'news/Information template',
-    value: 'dcloudio/uni-template-news'
-  },
-  {
-    name: 'custom template',
-    value: 'custom'
-  }
-  ],
   default: 'None'
 },
 {
